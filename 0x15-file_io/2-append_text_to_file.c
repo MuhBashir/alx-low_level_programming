a8b73fd39288734a92beee6294ff711636ca9345
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2022-12-05 14:09:27 -0800

    hat appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
  * append_text_to_file - that appends text at the end of a file
@@ -33,4 +33,5 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	write(file, text_content, i);
 
-	
+	return (1);
+}	
