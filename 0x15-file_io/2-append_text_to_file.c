f8539bd6fa268464ba38eafb1846aec49feaf3ae
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2022-12-05 14:13:45 -0800

    hat appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,39 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * append_text_to_file - that appends text at the end of a file
- * @filename: variable pointer
- * @text_content: content file
- * Description: function that appends text at the end of a file
- * Return: 1 on success, -1 on failure
+ * append_text_to_file - function with two arguments
+ * @filename: pointer to name of file
+ * @text_content: pointer to string content
+ *
+ * Description: function that appends to text at the end of a file
+ * Return: 1 on success or -1 for failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	int count = 0;
+	int output = 0;
+	int fd = 0;
 
-	if (filename == NULL)
+	if (filename != NULL)
+		fd = open(filename, O_WRONLY | O_APPEND);
+	else
+		return (-1);
+	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-
-	while (text_content[i] != '\0')
+	if (text_content != NULL)
 	{
-		i++;
+		while (text_content[count] != '\0')
+			count++;
 	}
+	else
+		return (1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
-
-	if (file == -1)
+	output = write(fd, text_content, count);
+	if (output == -1)
 		return (-1);
-
-	write(file, text_content, i);
-
+	close(fd);
 	return (1);
-}	
+}
